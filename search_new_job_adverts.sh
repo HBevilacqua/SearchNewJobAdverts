32b7bbf96d26f3bec9f89c911286f1eca357e4b2
HBevilacqua <hugo.benjamin.bevilacqua@gmail.com>
2016-10-10 08:46:56 +0000

    change file names

--- search_new_job_adverts.sh
+++ search_new_job_adverts.sh
@@ -1,14 +1,16 @@
-if [ ! -f previous_nb_words.txt ]; then
-    touch previous_nb_words.txt
-    nb_lines=$(cat webpages.txt | wc -l)
+# -------- STEP 0  --------
+# create the file to store the number of key words
+if [ ! -f reg_nb_words.txt ]; then
+    touch reg_nb_words.txt
+    nb_lines=$(cat list_webpages.txt | wc -l)
     for idx in $(seq 0 $nb_lines); do
-        echo "0" >> previous_nb_words.txt
+        echo "0" >> reg_nb_words.txt
     done
 fi
+# test purpose:
+#cat reg_nb_words.txt
 
-cat  previous_nb_words.txt
-
-# -------- STEP 0 --------
+# -------- STEP 1 --------
 # build the set of words to scan
 idx=0
 while read word; do
@@ -18,21 +20,22 @@ while read word; do
         key_words="$key_words\|$word"
     fi
     idx=$(($idx+1))
-    done < keywords.txt
+    done < list_keywords.txt
 
-echo ----- key words: $key_words
+# test purpose:
+#echo ----- key words: $key_words
 
-# -------- STEP 1 -------- 
-# update the previousNbWord array with the previous number 
+# -------- STEP 2 --------
+# update the previousNbWord array with the previous number
 # of key words found per web pages
 idx=0
 while read previous_nb_word; do
     previousNbWord[$idx]=$previous_nb_word
     idx=$(($idx+1))
-done < previous_nb_words.txt
+done < reg_nb_words.txt
 
-# -------- STEP 2 --------
-# for each web page of the webpages.txt file
+# -------- STEP 3 --------
+# for each web page of the list_webpages.txt file
 # looking for the key words in its content
 idx=0
 while read line_content; do
@@ -43,42 +46,34 @@ while read line_content; do
     echo $web_page > ${PWD}/log.txt
 
     # arg?
-    if [ -z "$1" ]; then
-        echo .
-    else
+    if [ ! -z "$1" ]; then
         # verbose ?
         if [ $1 = "-v" ]; then
             echo $web_page | grep --color=always $key_words
-        else
-            echo .
         fi
     fi
 
     # find and count the number of key words into the current web page
-    nb_words[$idx]=$(grep -o $key_words ${PWD}/log.txt | wc -l)
+    currentNbWords[$idx]=$(grep -o $key_words ${PWD}/log.txt | wc -l)
 
     # test the current and the previous number of words found
-    if [ ${nb_words[$idx]} = ${previousNbWord[$idx]} ]; then
-        echo not found
-    else
-        echo found
+    if [ ! ${currentNbWords[$idx]} = ${previousNbWord[$idx]} ]; then
+        echo page to visit
     fi
 
     idx=$(($idx+1))
 
-    done < webpages.txt
+    done < list_webpages.txt
 
-# -------- STEP 3 -------- 
-# update the previous_nb_words.txt file with the number
+# -------- STEP 4 --------
+# update the reg_nb_words.txt file with the number
 # of key words found per web pages
-nb_lines=$(cat previous_nb_words.txt | wc -l)
-rm  previous_nb_words.txt
+nb_lines=$(cat reg_nb_words.txt | wc -l)
+rm  reg_nb_words.txt
 for idx in $(seq 0 $nb_lines); do
-    echo -n ${nb_words[$idx]} >> previous_nb_words.txt
+    echo -n ${currentNbWords[$idx]} >> reg_nb_words.txt
     # avoid to add a line at the end of the file
     if [ $idx -ne $(($nb_lines-1)) ]; then
-        echo -n -e "\n" >> previous_nb_words.txt
+        echo -n -e "\n" >> reg_nb_words.txt
     fi
 done
-
-echo ----- end
